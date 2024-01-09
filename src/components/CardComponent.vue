ab96e3087e07016c666185546820c552bc1beb2f
EmelineBourgois <emelinebourgois@gmail.com>
2024-01-09 12:38:56 +0100

    horizontaal

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -58,69 +58,70 @@ const onDrop = (toListId) => {
 
 <template>
   <div class="container">
-    <draggable v-model="store.cards" group="lists" :item-key="card => card.id" @start="onDragStart" @end="onDrop"
-               class="d-flex flex-wrap">
-      <template #item="{ element, index }">
-        <div :key="index" class="col-md-3 m-3 card-container">
-          <div class="card">
-            <div class="card-body bg-body-tertiary shadow rounded-2">
-              <div class="d-flex justify-content-between align-items-center mb-3">
-                <h4 v-if="!element.editingTitle" @click="editTitle(element)" class="clickable-title">
-                  {{ element.title ? element.title : 'Add Title' }}
-                </h4>
-                <input
-                    style="border: 1px solid #ccc; border-radius: 5px; padding: 5px; margin: 0; width: 100%; box-sizing: border-box;"
-                    v-show="element.editingTitle" v-model="element.title" @blur="saveTitle(element)"
-                    @keyup.enter="saveTitle(element)"/>
-                <i class="bi bi-pencil-square text-black bg-white"></i>
+    <div class="custom-wrapper">
+      <draggable v-model="store.cards" group="lists" :item-key="card => card.id" @start="onDragStart" @end="onDrop"
+                 class="d-flex flex-nowrap overflow-auto">
+        <template #item="{ element, index }">
+          <div :key="index" class=" m-3 card-container">
+            <div class="card" style="width: 300px">
+              <div class="card-body bg-body-tertiary shadow rounded-2">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                  <h4 v-if="!element.editingTitle" @click="editTitle(element)" class="clickable-title">
+                    {{ element.title ? element.title : 'Add Title' }}
+                  </h4>
+                  <input
+                      style="border: 1px solid #ccc; border-radius: 5px; padding: 5px; margin: 0; width: 100%; box-sizing: border-box;"
+                      v-show="element.editingTitle" v-model="element.title" @blur="saveTitle(element)"
+                      @keyup.enter="saveTitle(element)"/>
+                  <i class="bi bi-pencil-square text-black bg-white"></i>
 
-                <!-- Custom Dropdown -->
-                <div class="custom-dropdown" @click="toggleDropdown(element)">
-                  <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                         class="bi bi-three-dots" viewBox="0 0 16 16">
-                      <path
-                          d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
-                    </svg>
-                  </button>
-                  <ul :class="{ 'show': element.showDropdown }">
-                    <li @click="deleteCard(element.id)" class="text-danger">Delete</li>
-                  </ul>
+                  <!-- Custom Dropdown -->
+                  <div class="custom-dropdown" @click="toggleDropdown(element)">
+                    <button>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                           class="bi bi-three-dots" viewBox="0 0 16 16">
+                        <path
+                            d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
+                      </svg>
+                    </button>
+                    <ul :class="{ 'show': element.showDropdown }">
+                      <li @click="deleteCard(element.id)" class="text-danger">Delete</li>
+                    </ul>
+                  </div>
+                  <!-- End Custom Dropdown -->
+
                 </div>
-                <!-- End Custom Dropdown -->
+                <div class="todo-list">
+                  <ul class="list-group mt-2">
 
-              </div>
-              <div class="todo-list">
-                <ul class="list-group mt-2">
-
-                  <draggable v-model="element.todos" :group="'todos-'" @end="onDrop">
-                    <template v-slot:item="{ element: todo }">
-                      <li class="list-group-item" :class="{ 'list-group-item-success': todo.completed }">
-                        {{ todo.item }}
-                        <span class="actions float-end">
+                    <draggable v-model="element.todos" :group="'todos-'" @end="onDrop">
+                      <template v-slot:item="{ element: todo }">
+                        <li class="list-group-item" :class="{ 'list-group-item-success': todo.completed }">
+                          {{ todo.item }}
+                          <span class="actions float-end">
         <button class="btn btn-sm" @click="toggleCompleted(element.id, todo.id)">&#10004;</button>
         <button class="btn btn-sm text-danger" @click="deleteTodoItem(element.id, todo.id)">&#10060;</button>
       </span>
-                      </li>
-                    </template>
-                  </draggable>
+                        </li>
+                      </template>
+                    </draggable>
 
 
-
-                </ul>
-                <form @submit.prevent="addTodoItem(element)">
-                  <div class="d-flex align-items-center">
-                    <input class="form-control text-center my-auto" v-model="element.newTodo" type="text"
-                           placeholder="new task"/>
-                    <button class="btn btn-success mt-2 ms-2" type="submit">+</button>
-                  </div>
-                </form>
+                  </ul>
+                  <form @submit.prevent="addTodoItem(element)">
+                    <div class="d-flex align-items-center">
+                      <input class="form-control text-center my-auto" v-model="element.newTodo" type="text"
+                             placeholder="new task"/>
+                      <button class="btn btn-success mt-2 ms-2" type="submit">+</button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </template>
-    </draggable>
+        </template>
+      </draggable>
+    </div>
   </div>
 </template>
 
@@ -170,5 +171,13 @@ const onDrop = (toListId) => {
 .custom-dropdown ul.show {
   display: block;
 }
+
+.custom-wrapper {
+  display: flex;
+  overflow-x: auto;
+  margin: -10px; /* Adjust margin as needed */
+}
+
+
 </style>
 ```
